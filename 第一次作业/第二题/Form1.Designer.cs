215735be865cd4dd5b589ef5ba7f77998b92a1f7
Ouqifeizhouren <1014048176@qq.com>
2022-02-20 19:44:58 +0800

    main

--- 第一次作业/第二题/Form1.Designer.cs
+++ 第一次作业/第二题/Form1.Designer.cs
@@ -29,13 +29,99 @@ namespace 第二题
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.a = new System.Windows.Forms.TextBox();
+            this.b = new System.Windows.Forms.TextBox();
+            this.res = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // a
+            // 
+            this.a.Location = new System.Drawing.Point(58, 75);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(103, 21);
+            this.a.TabIndex = 0;
+            this.a.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // b
+            // 
+            this.b.Location = new System.Drawing.Point(199, 76);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(100, 21);
+            this.b.TabIndex = 1;
+            // 
+            // res
+            // 
+            this.res.Location = new System.Drawing.Point(321, 76);
+            this.res.Name = "res";
+            this.res.ReadOnly = true;
+            this.res.Size = new System.Drawing.Size(100, 21);
+            this.res.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(136, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "点击运算符开始运算";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(164, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(33, 20);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(301, 76);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(19, 21);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "=";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(452, 118);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.a);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.TextBox b;
+        private System.Windows.Forms.TextBox res;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- 第一次作业/第二题/Form1.cs
+++ 第一次作业/第二题/Form1.cs
@@ -16,5 +16,65 @@ namespace 第二题
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double A = Convert.ToDouble(a.Text);
+            double B = Convert.ToDouble(b.Text);
+            res.Text = Convert.ToString(A + B);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double A = Convert.ToDouble(a.Text);
+            double B = Convert.ToDouble(b.Text);
+            res.Text = Convert.ToString(A - B);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double A = Convert.ToDouble(a.Text);
+            double B = Convert.ToDouble(b.Text);
+            res.Text = Convert.ToString(A * B);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double A = Convert.ToDouble(a.Text);
+            double B = Convert.ToDouble(b.Text);
+            if (B == 0) res.Text = "被除数不能为0";
+            else res.Text = Convert.ToString(A / B);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double A = Convert.ToDouble(a.Text);
+            double B = Convert.ToDouble(b.Text);
+            switch (comboBox1.Text)
+            {
+                case "+": res.Text = Convert.ToString(A + B); break;
+                case "-": res.Text = Convert.ToString(A - B); break;
+                case "*": res.Text = Convert.ToString(A * B); break;
+                case "/":
+                    if (B == 0) res.Text = "被除数不能为0";
+                    else res.Text = Convert.ToString(A / B);
+                    break;
+            }
+        }
     }
 }
